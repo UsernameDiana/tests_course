e41da59c3b2d2702eb3cf1fc17a1fe6938f8b9cb
UsernameDiana <ms.strele@gmail.com>
2018-02-12 13:39:47 +0100

    fix zero input

--- 01_test_cases/triangles.py
+++ 01_test_cases/triangles.py
@@ -1,11 +1,22 @@
 print("Input lengths of the triangle sides: ")
-x = int(input("x: "))
-y = int(input("y: "))
-z = int(input("z: "))
 
-if x == y == z:
-    print("Equilateral triangle")
-elif x != y != z:
-    print("Scalene triangle")
-else:
-    print("isosceles triangle")
+
+def triangle_check(l1, l2, l3):
+    if (l1 > l2+l3) or (l2 > l1+l3) or ()\
+            or (l3 > l1+l2) or (l1 == 0) or (l2 == 0) or (l3 == 0):
+        print ('No, the values wont form a triangle')
+    else:
+        print ('Yes, a triangle can be formed out of it:')
+
+        if l1 == l2 == l3:
+            print("Equilateral triangle")
+        elif l1 != l2 != l3:
+            print("Scalene triangle")
+        else:
+            print("isosceles triangle")
+
+length1 = int(input('enter side a\n'))
+length2 = int(input('enter side b\n'))
+length3 = int(input('enter side c\n'))
+
+triangle_check(length1, length2, length3)
