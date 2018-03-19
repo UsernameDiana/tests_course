d449b2f2f6551c7f3dedf3897f5fbd007d4fdf70
UsernameDiana <ms.strele@gmail.com>
2018-03-19 18:03:19 +0100

    setup

---
+++ Midterm_Assignment/Testing Real Life Code/javaanpr-master/src/test/java/net/sf/javaanpr/test/RecognitionAllIT.java
@@ -0,0 +1,22 @@
+package net.sf.javaanpr.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author diana
+ */
+public class RecognitionAllIT {
+    
+    // test that will report green/red for each image being tested
+    
+    @Test
+    public void RecognitionAllIT() {
+    }
+    
+    // how images are read, 
+    // how expected results are read, 
+    // how to call the recognize method
+    
+}
